21744143bbf7a2ba0029f26add63762d2d7db34c
juykang <sk651726@gmail.com>
2023-05-29 17:49:15 +0900

    ex02 make

---
+++ ex02/Account.cpp
@@ -0,0 +1,57 @@
+#include "Account.hpp"
+
+int	Account::getNbAccounts(void)
+{
+
+}
+
+int	Account::getTotalAmount(void)
+{
+
+}
+
+int	Account::getNbDeposits(void)
+{
+
+}
+
+int	Account::getNbWithdrawals(void)
+{
+
+}
+
+void Account::displayAccountsInfos(void)
+{
+
+}
+
+Account::Account(int initial_deposit)
+{
+
+}
+
+Account::~Account(void)
+{
+
+}
+
+
+void Account::makeDeposit(int deposit)
+{
+
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+
+}
+
+int Account::checkAmount(void) const
+{
+
+}
+
+void Account::displayStatus(void)const
+{
+
+}
